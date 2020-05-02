5dad65d1bab307105569f38e4f329449c2ed2bb7
Alfredo Mariño <alfredoalejandro05@gmail.com>
2020-05-02 15:30:11 -0400

    persistence and service layer done

--- src/main/java/com/aamv/mybookql/MyBooksQLApplication.java
+++ src/main/java/com/aamv/mybookql/MyBooksQLApplication.java
@@ -1,54 +1,13 @@
 package com.aamv.mybookql;
 
-import com.aamv.mybookql.model.Author;
-import com.aamv.mybookql.model.Book;
-import com.aamv.mybookql.service.AuthorService;
-import com.aamv.mybookql.service.BookService;
-import com.google.cloud.firestore.Firestore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
 public class MyBooksQLApplication {
 
-//	@Autowired
-	static Firestore firestore;
-
-//	@Autowired
-	static AuthorService authorService;
-	static BookService bookService;
-
-	public static void main(String[] args) throws ExecutionException, InterruptedException {
-		ConfigurableApplicationContext context = SpringApplication.run(MyBooksQLApplication.class, args);
-
-		firestore = (Firestore) context.getBean("firestore");
-		authorService = (AuthorService) context.getBean("authorServiceImpl");
-		bookService = (BookService) context.getBean("bookServiceImpl");
-
-		Author author = new Author();
-		author.setId(UUID.randomUUID().toString());
-		author.setName("Alfredo Mariño");
-
-		Author newAuthor = authorService.createAuthor(author);
-
-
-		Book book = new Book();
-		book.setId(UUID.randomUUID().toString());
-		book.setTitle("Libro 1");
-
-		List<Author> authorList = new ArrayList<>();
-		authorList.add(newAuthor);
-		book.setAuthors(authorList);
-
-		bookService.createBook(book);
-
-
+	public static void main(String[] args) {
+		SpringApplication.run(MyBooksQLApplication.class, args);
 	}
 
 }
